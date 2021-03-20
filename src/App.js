238705feb17da94e668b30aae886a804d376e342
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-03-20 21:47:56 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Fot from "./component/footer";
+function App() {
+  return (
+    <div className="App">
+      <Fot/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/about.js
@@ -0,0 +1,16 @@
+//into context + style 
+// vision and mission 
+import React, { Component } from "react";
+
+export class About extends Component {
+  render() {
+    return (
+        <div calssName="container">
+
+          <button calssName="button"> about  ???? </button>
+        </div>
+    );
+}
+}
+
+export default About;
